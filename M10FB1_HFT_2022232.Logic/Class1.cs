b042a6cb0dbc65bf3b840d65f5136f3979ae22e0
nagybenc <nagybence105@gmail.com>
2023-04-23 22:55:21 +0200

    First milestone done

--- M10FB1_HFT_2022232.Logic/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace M10FB1_HFT_2022232.Logic
-{
-    public class Class1
-    {
-    }
-}
--- M10FB1_HFT_2022232.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace M10FB1_HFT_2022232.Test
-{
-    public class Class1
-    {
-    }
-}
