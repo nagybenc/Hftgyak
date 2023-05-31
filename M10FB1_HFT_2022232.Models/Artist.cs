fd8f20262908895ccb0f7aa837d23def701184ed
nagybenc <nagybence105@gmail.com>
2023-05-31 14:17:08 +0200

    Artist class added

---
+++ M10FB1_HFT_2022232.Models/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Models
+{
+    class Artist
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public virtual ICollection<Album> Albums { get; set; }
+        public Artist()
+        {
+            Albums = new List<Album>();
+        }
+    }
+}
