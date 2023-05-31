1d82010c37802e0bee5e133b57a5f5d24de5b4b6
nagybenc <nagybence105@gmail.com>
2023-05-31 14:06:07 +0200

    Label class added

---
+++ M10FB1_HFT_2022232.Models/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Models
+{
+    class Album
+    {
+    }
+}
---
+++ M10FB1_HFT_2022232.Models/Label.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Models
+{
+    class Label
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        [NotMapped]
+        public virtual ICollection<Album> Albums { get; set; }
+    }
+}
