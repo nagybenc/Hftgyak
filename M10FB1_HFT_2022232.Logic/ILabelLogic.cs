7d484eac4a2ce783650bd9fd8094bf0d4463dc7e
nagybenc <nagybence105@gmail.com>
2023-06-09 16:07:02 +0200

    ILabelLogic  interface created

---
+++ M10FB1_HFT_2022232.Logic/ILabelLogic.cs
@@ -0,0 +1,20 @@
+﻿using M10FB1_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Logic
+{
+    public interface ILabelLogic
+    {
+        void Create(Label item);
+        void Delete(int id);
+        Label Read(int id);
+        IQueryable<Label> ReadAll();
+        void Update(Label item);
+
+        //IQueryable;
+    }
+}
