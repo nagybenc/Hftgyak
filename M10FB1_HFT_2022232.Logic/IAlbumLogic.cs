55511c999eab5d590173c73c14f37d996b68068f
nagybenc <nagybence105@gmail.com>
2023-06-09 16:10:26 +0200

    IAlbumLogic interface created

---
+++ M10FB1_HFT_2022232.Logic/IAlbumLogic.cs
@@ -0,0 +1,18 @@
+﻿using M10FB1_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Logic
+{
+    public interface IAlbumLogic
+    {
+        void Create(Album item);
+        void Delete(int id);
+        Album Ream(int id);
+        IQueryable<Album> ReadAll();
+        void Update(Album item);
+    }
+}
