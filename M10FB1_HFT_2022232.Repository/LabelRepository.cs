4da8e0b30a74e30d5b084ec32f41b93eee12b204
nagybenc <nagybence105@gmail.com>
2023-06-09 12:04:22 +0200

    LabelRepository created for Label model

---
+++ M10FB1_HFT_2022232.Repository/LabelRepository.cs
@@ -0,0 +1,30 @@
+﻿using M10FB1_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Repository
+{
+    public class LabelRepository : Repository<Label>
+    {
+        public LabelRepository(MusicDbContext mdb):base (mdb)
+        { }
+
+        public override Label Read(int id)
+        {
+            return mdb.Labels.FirstOrDefault(t => t.Id == id);
+        }
+
+        public override void Update(Label item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            mdb.SaveChanges();
+        }
+    }
+}
