b04c9ef55821cea39048d35132c4906a2d4f3c5d
nagybenc <nagybence105@gmail.com>
2023-06-10 10:07:44 +0200

    ArtistLogic created, implemented IArtistLogic interface

---
+++ M10FB1_HFT_2022232.Logic/ArtistLogic.cs
@@ -0,0 +1,43 @@
+﻿using M10FB1_HFT_2022232.Models;
+using M10FB1_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Logic
+{
+    public class ArtistLogic : IArtistLogic
+    {
+        IRepository<Artist> repository;
+        public ArtistLogic(IRepository<Artist> repository)
+        {
+            this.repository = repository;
+        }
+        public void Create(Artist item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Artist Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<Artist> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Artist item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
