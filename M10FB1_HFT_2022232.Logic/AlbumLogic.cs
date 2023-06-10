adc031fb84111f3cb019aad32c9e6e5c11133708
nagybenc <nagybence105@gmail.com>
2023-06-10 10:13:56 +0200

    AlbumLogic created, IAlbumlogic interface implemented

---
+++ M10FB1_HFT_2022232.Logic/AlbumLogic.cs
@@ -0,0 +1,44 @@
+﻿using M10FB1_HFT_2022232.Models;
+using M10FB1_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Logic
+{
+    public class AlbumLogic : IAlbumLogic
+    {
+        IRepository<Album> repository;
+        public AlbumLogic(IRepository<Album> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Album item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public IQueryable<Album> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public Album Ream(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public void Update(Album item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
