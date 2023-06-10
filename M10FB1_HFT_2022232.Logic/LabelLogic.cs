23af4c6183f24e55b6fe2e8d09d5e223e36a8347
nagybenc <nagybence105@gmail.com>
2023-06-10 11:17:59 +0200

    LabelLogic created, ILabelLogic interface implemented

---
+++ M10FB1_HFT_2022232.Logic/LabelLogic.cs
@@ -0,0 +1,45 @@
+﻿using M10FB1_HFT_2022232.Models;
+using M10FB1_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M10FB1_HFT_2022232.Logic
+{
+    public class LabelLogic : ILabelLogic
+    {
+        IRepository<Label> repository;
+
+        public LabelLogic(IRepository<Label> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Create(Label item)
+        {
+            this.repository.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repository.Delete(id);
+        }
+
+        public Label Read(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        public IQueryable<Label> ReadAll()
+        {
+            return this.repository.ReadAll();
+        }
+
+        public void Update(Label item)
+        {
+            this.repository.Update(item);
+        }
+    }
+}
